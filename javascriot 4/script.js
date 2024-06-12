ab27450c67fd67b99ac0f7e69bf99f27dfb29ab5
Sida14 <164236903+Sida14@users.noreply.github.com>
2024-06-12 14:31:26 +0400

    Add files via upload

---
+++ javascriot 4/script.js
@@ -0,0 +1,57 @@
+let mk = 10
+
+switch (mk) {
+    case 10:
+        console.log ("es cvladi aris 10")
+        break;
+    case 11:
+        console.log ("es cvladi aris 11")
+        break;
+    case 12:
+        console.log ("es cvladi aris 12")
+        break;
+    case 13:
+        console.log ("es cvladi aris 13")
+        break;
+    case 14:
+        console.log ("es cvladi aris 14")
+        break;
+
+    default:
+        console.log ("es cvladi aris 15")
+}
+
+
+
+
+
+
+
+
+
+let sd = ["levani","meko","giorgi","mamuka","gega"]
+
+sd.shift ()
+sd.push ("levani")
+
+if (sd [4] == "levani") {
+    console.log ("levani")
+} 
+
+else if (sd [4] == "meko") {
+    console.log ("meko")
+}
+
+else if (sd [4] == "giorgi") {
+    console.log ("giorgi")
+}
+
+else if (sd [4] == "mamuka") {
+    console.log ("mamuka")
+}
+
+
+else {
+    console.log ("gega")
+}
+
